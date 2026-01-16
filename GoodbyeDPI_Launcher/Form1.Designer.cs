3876694f3fb53871c81267b580a9fe91056808c8
Salim STYLE <156449458+Salim6996@users.noreply.github.com>
2026-01-16 20:08:30 +0300

    Add files via upload

---
+++ GoodbyeDPI_Launcher/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace GoodbyeDPI_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnKaldir = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnKaldir);
+            this.groupBox1.Controls.Add(this.btnDurdur);
+            this.groupBox1.Controls.Add(this.btnBaslat);
+            this.groupBox1.Location = new System.Drawing.Point(28, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(920, 403);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GoodbyeDPI";
+            // 
+            // btnKaldir
+            // 
+            this.btnKaldir.BackColor = System.Drawing.Color.Red;
+            this.btnKaldir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaldir.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnKaldir.Location = new System.Drawing.Point(228, 263);
+            this.btnKaldir.Name = "btnKaldir";
+            this.btnKaldir.Size = new System.Drawing.Size(519, 73);
+            this.btnKaldir.TabIndex = 2;
+            this.btnKaldir.Text = "KALDIR";
+            this.btnKaldir.UseVisualStyleBackColor = false;
+            this.btnKaldir.Click += new System.EventHandler(this.btnKaldir_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.BackColor = System.Drawing.Color.DimGray;
+            this.btnDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurdur.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnDurdur.Location = new System.Drawing.Point(228, 172);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(519, 73);
+            this.btnDurdur.TabIndex = 1;
+            this.btnDurdur.Text = "DURDUR";
+            this.btnDurdur.UseVisualStyleBackColor = false;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.BackColor = System.Drawing.Color.Green;
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslat.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnBaslat.Location = new System.Drawing.Point(228, 81);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(519, 73);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "BAŞLAT";
+            this.btnBaslat.UseVisualStyleBackColor = false;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(506, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Aşağıdaki butonlardan DPI servisini yönetebilirsiniz.";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 490);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(120, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(810, 587);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Launcher by: salim_style";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(256, 477);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(138, 495);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(115, 95);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 621);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GoodbyeDPI Launcher";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnKaldir;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ GoodbyeDPI_Launcher/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using IWshRuntimeLibrary;
+using System.Security.Principal;
+using System.Reflection;
+
+namespace GoodbyeDPI_Launcher
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] resourceFiles = { "goodbyedpi.exe", "WinDivert.dll", "WinDivert64.sys" };
+        private readonly string tempDir = Path.Combine(Path.GetTempPath(), "GoodbyeDPI");
+        private NotifyIcon trayIcon;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupTrayIcon();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Contains("-autostart"))
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+                this.Visible = false;
+                StartDpiProcess(true);
+            }
+        }
+
+        private void SetupTrayIcon()
+        {
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "GoodbyeDPI Launcher";
+            trayIcon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Göster", null, (s, e) => ShowForm());
+            contextMenu.Items.Add("Kapat", null, (s, e) => {
+                btnDurdur_Click(null, null);
+                Application.Exit();
+            });
+
+            trayIcon.ContextMenuStrip = contextMenu;
+            trayIcon.Visible = true;
+            trayIcon.DoubleClick += (s, e) => ShowForm();
+        }
+
+        private void ShowForm()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            this.BringToFront();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://discord.gg/Xbk6GYyxX8",
+                UseShellExecute = true
+            });
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            StartDpiProcess(false);
+        }
+
+        private void StartDpiProcess(bool silentMode)
+        {
+            try
+            {
+                Process[] runningProcesses = Process.GetProcessesByName("goodbyedpi");
+                foreach (Process process in runningProcesses)
+                {
+                    if (process.Id != Process.GetCurrentProcess().Id)
+                    {
+                        if (!silentMode) MessageBox.Show("GoodbyeDPI zaten çalışıyor!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+                }
+
+                if (!ExtractResources())
+                {
+                    if (!silentMode) MessageBox.Show("Gerekli dosyalar kaynaklardan çıkarılamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string exePath = Path.Combine(tempDir, "goodbyedpi.exe");
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = exePath,
+                    Arguments = "-5 --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253",
+                    UseShellExecute = true,
+                    Verb = "runas",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    WorkingDirectory = tempDir
+                };
+
+                Process.Start(psi);
+                RegisterTask();
+
+                if (!silentMode)
+                    MessageBox.Show("GoodbyeDPI başlatıldı.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    trayIcon.ShowBalloonTip(3000, "GoodbyeDPI", "Arka planda otomatik başlatıldı.", ToolTipIcon.Info);
+            }
+            catch (Exception ex)
+            {
+                if (!silentMode) MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RegisterTask()
+        {
+            try
+            {
+                string taskName = "GoodbyeDPILauncherAutoStart";
+                string exePath = Application.ExecutablePath;
+                string args = "-autostart";
+
+                string xmlCommand = $"/Create /TN \"{taskName}\" /TR \"'{exePath}' {args}\" /SC ONLOGON /RL HIGHEST /F";
+
+                ProcessStartInfo psi = new ProcessStartInfo("schtasks.exe", xmlCommand)
+                {
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+                Process.Start(psi);
+            }
+            catch { }
+        }
+
+        private bool ExtractResources()
+        {
+            try
+            {
+                if (!Directory.Exists(tempDir)) Directory.CreateDirectory(tempDir);
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                string[] allResourceNames = assembly.GetManifestResourceNames();
+
+                foreach (string fileName in resourceFiles)
+                {
+                    string targetPath = Path.Combine(tempDir, fileName);
+                    if (System.IO.File.Exists(targetPath) && new FileInfo(targetPath).Length > 0) continue;
+                    string resourceName = allResourceNames.FirstOrDefault(r => r.EndsWith(fileName, StringComparison.InvariantCultureIgnoreCase));
+                    if (string.IsNullOrEmpty(resourceName)) return false;
+
+                    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                    {
+                        if (stream == null) return false;
+                        using (FileStream fileStream = System.IO.File.Create(targetPath))
+                        {
+                            stream.CopyTo(fileStream);
+                        }
+                    }
+                }
+                return true;
+            }
+            catch { return false; }
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName("goodbyedpi");
+                bool stopped = false;
+                foreach (Process p in processes)
+                {
+                    p.Kill();
+                    p.WaitForExit();
+                    stopped = true;
+                }
+
+                if (sender != null)
+                {
+                    if (stopped) MessageBox.Show("GoodbyeDPI durduruldu.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    else MessageBox.Show("Çalışan GoodbyeDPI bulunamadı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (sender != null) MessageBox.Show($"Durdurma hatası: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnKaldir_Click(object sender, EventArgs e)
+        {
+            if (!IsAdministrator())
+            {
+                MessageBox.Show("Yönetici olarak çalıştırmalısınız!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (MessageBox.Show("Sistem temizlenecek, onaylıyor musunuz?", "Onay", MessageBoxButtons.YesNo) == DialogResult.No) return;
+
+            try
+            {
+                Process.Start(new ProcessStartInfo("schtasks.exe", "/Delete /TN \"GoodbyeDPILauncherAutoStart\" /F") { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden });
+
+                StopAndDeleteService("GoodbyeDPI");
+                StopAndDeleteService("WinDivert");
+                foreach (var p in Process.GetProcessesByName("goodbyedpi")) { try { p.Kill(); } catch { } }
+
+                if (Directory.Exists(tempDir)) Directory.Delete(tempDir, true);
+                if (trayIcon != null) trayIcon.Visible = false;
+
+                MessageBox.Show("Sistem temizlendi.");
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void StopAndDeleteService(string serviceName)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("sc.exe", $"stop \"{serviceName}\"") { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden })?.WaitForExit();
+                Process.Start(new ProcessStartInfo("sc.exe", $"delete \"{serviceName}\"") { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden })?.WaitForExit();
+            }
+            catch { }
+        }
+
+        private bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                return new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("GoodbyeDPI Launcher\nSürüm: v1.0.0", "Bilgi");
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo { FileName = "https://github.com/Salim6996/GoodbyeDPI-Launcher", UseShellExecute = true });
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (trayIcon != null)
+            {
+                trayIcon.Visible = false;
+                trayIcon.Dispose();
+            }
+            base.OnFormClosing(e);
+        }
+    }
+}
